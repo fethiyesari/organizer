823961a45ce87863ac9e566c6b3c40b0495d38a5
ibrahimbinbuga <ibrahimm3334@icloud.com>
2024-12-09 22:02:17 +0300

    minor fix at notes page

--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:organizer/components/custom_drawer.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -18,21 +19,29 @@ class _NotesPageState extends State<NotesPage> {
   final TextEditingController _contentController = TextEditingController();
 
   Future<void> _addNote() async {
-    if (_titleController.text.isNotEmpty &&
-        _contentController.text.isNotEmpty) {
-      try {
-        await notesCollection.add({
-          'userId': user.uid,
-          'title': _titleController.text,
-          'content': _contentController.text,
-          'timestamp': FieldValue.serverTimestamp(),
-        });
-        _titleController.clear();
-        _contentController.clear();
-        Navigator.pop(context);
-      } catch (e) {
-        print("Error adding note: $e");
-      }
+    if (_titleController.text.isEmpty || _contentController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Title and content cannot be null.")),
+      );
+      return;
+    }
+    try {
+      await notesCollection.add({
+        'userId': user.uid,
+        'title': _titleController.text,
+        'content': _contentController.text,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      _titleController.clear();
+      _contentController.clear();
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Note added successfully.")),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error adding note: $e")),
+      );
     }
   }
 
@@ -40,29 +49,31 @@ class _NotesPageState extends State<NotesPage> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: const Text('Yeni Not Ekle'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Başlık'),
-            ),
-            TextField(
-              controller: _contentController,
-              decoration: const InputDecoration(labelText: 'İçerik'),
-              maxLines: 3,
-            ),
-          ],
+        title: const Text('Add New Note'),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+              ),
+              TextField(
+                controller: _contentController,
+                decoration: const InputDecoration(labelText: 'Content'),
+                maxLines: 3,
+              ),
+            ],
+          ),
         ),
         actions: [
           TextButton(
             onPressed: () => Navigator.pop(context),
-            child: const Text('İptal'),
+            child: const Text('Cancel'),
           ),
           ElevatedButton(
             onPressed: _addNote,
-            child: const Text('Ekle'),
+            child: const Text('Add'),
           ),
         ],
       ),
@@ -72,8 +83,13 @@ class _NotesPageState extends State<NotesPage> {
   Future<void> _deleteNote(DocumentSnapshot note) async {
     try {
       await notesCollection.doc(note.id).delete();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Note deleted successfully.")),
+      );
     } catch (e) {
-      print("Error deleting note: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error deleting note: $e")),
+      );
     }
   }
 
@@ -81,10 +97,11 @@ class _NotesPageState extends State<NotesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notlar'),
+        title: const Text('Notes'),
         toolbarHeight: 80,
         backgroundColor: Colors.deepOrange,
       ),
+      drawer: const CustomDrawer(),
       body: StreamBuilder<QuerySnapshot>(
         stream: notesCollection
             .where('userId', isEqualTo: user.uid)
@@ -96,7 +113,7 @@ class _NotesPageState extends State<NotesPage> {
           }
           final notes = snapshot.data!.docs;
           if (notes.isEmpty) {
-            return const Center(child: Text('Henüz not eklenmedi.'));
+            return const Center(child: Text('No notes added yet.'));
           }
           return ListView.builder(
             itemCount: notes.length,
